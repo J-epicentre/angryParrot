dd7490376f9ba8b1ba831bb31686486a64de3b87
wldyd428 <78496952+wldyd428@users.noreply.github.com>
2024-08-21 20:49:51 +0900

    join 수정

--- src/main/java/com/project2/angry_parrot/controller/RestApiController.java
+++ src/main/java/com/project2/angry_parrot/controller/RestApiController.java
@@ -2,7 +2,10 @@ package com.project2.angry_parrot.controller;
 
 import com.project2.angry_parrot.model.User;
 import com.project2.angry_parrot.repository.UserRepository;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,12 +34,27 @@ public class RestApiController {
         return "<h1>token</h1>";
     }
 
+//    @PostMapping("join")
+//    public String join(@RequestBody User user) {
+//        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+//        user.setRoles("ROLE_USER");
+//        userRepository.save(user);
+//        return "회원가입완료";
+//    }
+
     @PostMapping("join")
-    public String join(@RequestBody User user) {
+    public ResponseEntity<Map<String, Object>> join(@RequestBody User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setRoles("ROLE_USER");
         userRepository.save(user);
-        return "회원가입완료";
+
+        // 응답으로 보낼 JSON 데이터를 준비
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "회원가입완료");
+        response.put("userId", user.getId()); // 필요한 다른 정보 추가 가능
+
+        // JSON 응답 반환
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/api/v1/user")
